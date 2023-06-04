2f21a2f4bcbb3aededd9f54932d2795edfb41d89
Alexander Ramírez Chiquito <alexander.ramirezc@unac.edu.co>
2023-06-04 15:37:49 -0500

    change a screen home

--- App.js
+++ App.js
@@ -2,11 +2,12 @@
 // import Landing from './components/auth/Landing';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './components/auth/AuthStack';
+import HomeStackNavigator from './navigations/Navigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <AuthStack/>
+      < HomeStackNavigator/>
     </NavigationContainer>
   );
 }
