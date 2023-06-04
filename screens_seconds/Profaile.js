ddd6dc91a94a70a667d9accd761ce110fb59302e
Alexander Ramírez Chiquito <alexander.ramirezc@unac.edu.co>
2023-06-04 15:42:57 -0500

    Create screen profile :3

---
+++ screens_seconds/Profaile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+const Profile = () =>{
+    return(
+        <View style={{justifyContent:"center",
+                      alignItems:"center",
+                      flex:1,
+        }} >
+            <Text>ppp</Text>
+        </View>
+    )
+}
+
+export default Profile;
