c5d2f16dfff953c854c45d677b817099ba27d43e
Said Elias Salcedo Cupitra <ssaidsal08@gmail.com>
2023-06-05 11:40:26 -0500

    amplifyExecution

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import { SafeAreaView, StyleSheet } from 'react-native';
 import Navigation from './navigation';
 import React from 'react';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native'
+import config from './src/aws-exports'
+
+Amplify.configure(config);
 
 const App = () => {
   return (
