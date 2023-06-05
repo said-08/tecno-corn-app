e6709ee83ab4acf21cd934c111f1fbbb38251602
Alexander Ramírez Chiquito <alexander.ramirezc@unac.edu.co>
2023-06-05 07:46:55 -0500

    Holaa bro ;:3

--- App.js
+++ App.js
@@ -2,12 +2,11 @@
 // import Landing from './components/auth/Landing';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthStack from './components/auth/AuthStack';
-import HomeStackNavigator from './navigations/Navigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      < HomeStackNavigator/>
+      <AuthStack/>
     </NavigationContainer>
   );
 }
